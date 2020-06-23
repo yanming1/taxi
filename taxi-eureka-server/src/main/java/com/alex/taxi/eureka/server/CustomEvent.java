9a900f5aaefcfcaa492b73e102923c42b7054d4b
Alex <yanmingxuehu@163.com>
2020-06-23 20:51:44 +0800

    [Add] eureka-server

---
+++ taxi-eureka-server/src/main/java/com/alex/taxi/eureka/server/CustomEvent.java
@@ -0,0 +1,14 @@
+package com.alex.taxi.eureka.server;
+
+import org.springframework.cloud.netflix.eureka.server.event.EurekaInstanceCanceledEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomEvent {
+    @EventListener
+    public void listen(EurekaInstanceCanceledEvent e) {
+        System.out.println(e.getServerId()+"下线事件");
+        //发送邮件，短信，电话。
+    }
+}
